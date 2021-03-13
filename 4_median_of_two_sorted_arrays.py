363956617d6e0050ccaf4e68e22c2002846f96a8
Muwu-Xu <muwu.xu@gmail.com>
2021-03-13 14:44:36 -0500

    change of 4

--- 4_median_of_two_sorted_arrays.py
+++ 4_median_of_two_sorted_arrays.py
@@ -1,14 +1,41 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-    
-    ##brute force
-            s = nums1 + nums2
         
-            s.sort()
+        A,B = nums1,nums2
+        
+        m, n = len(nums1), len(nums2)
+        
+        if m > n: 
+            A, B, m, n = B, A, n, m
+        if n == 0:
+            raise ValueError 
+        
+        i_min, i_max, half_len = 0, m, (m+n+1) // 2 
+        
+        while i_min <= i_max: 
+            i = (i_min + i_max) // 2 
+            j = half_len - i 
             
-            n = len(s)
+            if i<m and B[j-1] > A[i]:
+                i_min = i + 1
             
-            if  n % 2 == 1: 
-                return s[int(n/2)]
+            elif i > 0 and A[i-1] > B[j]:
+                
+                i_max = i - 1 
+            
             else: 
-                return (s[len(s)//2-1] + s[len(s)//2]) /2
+                # i is good 
+                
+                if i ==0: max_of_left = B[j-1]
+                elif j ==0: max_of_left = A[i-1]
+                else: max_of_left = max(A[i-1],B[j-1])
+                    
+                if (m+n) % 2 == 1:
+                    return max_of_left
+                
+                if i == m: min_of_right = B[j]
+                elif j == n: min_of_right = A[i]
+                else: min_of_right = min(A[i],B[j])
+                    
+                return (max_of_left + min_of_right ) / 2 
+                
